6586521c67febd8300fc06955faecf76c403264e
dukky <andy.duks@gmail.com>
2013-05-16 20:14:44 +0100

    Created Board and Cell classes

---
+++ src/im/duk/TicTacToe/Board.java
@@ -0,0 +1,77 @@
+package im.duk.TicTacToe;
+
+public class Board {
+	public static final int ROWS = 3;
+	public static final int COLS = 3;
+
+	public Cell[][] cells;
+
+	public Board() {
+		init();
+	}
+
+	private void init() {
+		cells = new Cell[ROWS][COLS];
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLS; j++) {
+				cells[i][j] = new Cell(i, j);
+			}
+		}
+	}
+
+	public boolean isDraw() {
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < ROWS; j++) {
+				if (cells[i][j].getContent() == Contents.EMPTY) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public boolean hasWon(Contents side, int row, int col) {
+		
+		// 3 in the row
+		if (cells[row][0].getContent() == side
+				&& cells[row][1].getContent() == side
+				&& cells[row][2].getContent() == side) {
+			return true;
+		//3 in the column
+		} else if (cells[0][col].getContent() == side
+				&& cells[1][col].getContent() == side
+				&& cells[2][col].getContent() == side) {
+			return true;
+		// 3 in the diagonal
+		} else if (row == col) {
+			if (cells[0][0].getContent() == side
+					&& cells[1][1].getContent() == side
+					&& cells[2][2].getContent() == side) {
+				return true;
+			}
+		// 3 in the other diagonal
+		} else if (row + col == 2) {
+			if(cells[0][2].getContent() == side
+					&& cells[1][1].getContent() == side
+					&& cells[2][0].getContent() == side) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+	
+	public void paint() {
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLS; j++) {
+				cells[i][j].paint();
+				System.out.print((j < COLS) ? "|" : "");
+			}
+			System.out.println();
+			if(i < ROWS - 1) {
+				System.out.println("-----------");
+			}
+		}
+	}
+
+}
---
+++ src/im/duk/TicTacToe/Cell.java
@@ -0,0 +1,57 @@
+package im.duk.TicTacToe;
+
+public class Cell {
+
+	private Contents content;
+	private int row;
+	private int col;
+
+	public Cell(int row, int col) {
+		this.row = row;
+		this.col = col;
+		clear();
+	}
+	
+	public void clear() {
+		this.content = Contents.EMPTY;
+	}
+
+	public void paint() {
+		switch (content) {
+		case EMPTY:
+			System.out.print("   ");
+			break;
+		case CROSS:
+			System.out.print(" X ");
+			break;
+		case NOUGHT:
+			System.out.print(" O ");
+			break;
+		}
+	}
+
+	public Contents getContent() {
+		return content;
+	}
+
+	public void setContent(Contents content) {
+		this.content = content;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+	public void setCol(int col) {
+		this.col = col;
+	}
+
+}
